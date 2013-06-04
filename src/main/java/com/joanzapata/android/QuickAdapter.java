43c7e9c3cf7c05799d409c4432745cf295aaacad
Joan Zapata <jzapata@excilys.com>
2013-06-04 15:52:50 +0200

    Base of quick adapter

---
+++ src/main/java/com/joanzapata/android/QuickAdapter.java
@@ -0,0 +1,87 @@
+package com.joanzapata.android;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.joanzapata.android.BaseAdapterHelper.get;
+
+/**
+ * Abstraction class of a BaseAdapter in which you only need
+ * to provide the convert() implementation.<br/>
+ * Using the provided BaseAdapterHelper, your code is minimalist.
+ * @param <T> The type of the items in the list.
+ */
+public abstract class QuickAdapter<T> extends BaseAdapter {
+
+    private final Context context;
+
+    private final int layoutResId;
+
+    private final List<T> data;
+
+    /**
+     * Create a QuickAdapter.
+     * @param context     The context.
+     * @param layoutResId The layout resource id of each item.
+     */
+    public QuickAdapter(Context context, int layoutResId) {
+        this(context, layoutResId, null);
+    }
+
+    /**
+     * Same as QuickAdapter#QuickAdapter(Context,int) but with
+     * some initialization data.
+     * @param context     The context.
+     * @param layoutResId The layout resource id of each item.
+     * @param data        A new list is created out of this one to avoid mutable list
+     */
+    public QuickAdapter(Context context, int layoutResId, List<T> data) {
+        this.data = new ArrayList<T>(data);
+        this.context = context;
+        this.layoutResId = layoutResId;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public T getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final BaseAdapterHelper helper = get(context, convertView, parent, layoutResId);
+        convert(helper, getItem(position));
+        return helper.getView();
+    }
+
+    public void add(T elem) {
+        data.add(elem);
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<T> elem) {
+        data.addAll(elem);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Implement this method and use the helper to adapt the view to the given item.
+     * @param helper A fully initialized helper.
+     * @param item   The item that needs to be displayed.
+     */
+    protected abstract void convert(BaseAdapterHelper helper, T item);
+}
